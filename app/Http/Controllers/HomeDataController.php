ebed2dde905866af58f664a1b0655a44496b48fb
unknown <iiurydias@hotmail.com>
2019-08-05 19:05:42 -0300

    ajustes

--- app/Http/Controllers/HomeDataController.php
+++ app/Http/Controllers/HomeDataController.php
@@ -21,6 +21,12 @@ class HomeDataController extends Controller
         $data = $request->all();
         $user = User::where('api_token', $data['token'])->first();
         $favorites = Favorite::where('fk_user', $user->id)->with("point")->get();
+        $favorites->map(function ($item) {
+            $point =  Point::where('id', $item->fk_point)->get()->first();
+            $category =  Category::where('id', $point->fk_category)->get()->first();
+            $item['icon'] = $category->icon;
+            return $item;
+          });
         $dados['categories'] = $categories->toArray();
         $dados['favorites'] = $favorites->toArray();
         return Functions::sendResponse($dados, "");
